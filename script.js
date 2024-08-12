6e7f0c34600926994fc87264e8d6908c02417fa0
Shivane Rana <shivanerana2004@gmail.com>
2024-08-12 16:01:44 +0530

    fromChecked added

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ const authorD = document.querySelector("#authorD");
 const titleD = document.querySelector("#titleD");
 const pagesD = document.querySelector("#pagesD");
 const itemContainer = document.querySelector(".itemContainer");
-const remove = document.createElement("button");
-const status = document.createElement("button");
+const remove = document.querySelector(".remove");
+const read = document.querySelector(".read");
+const item = document.querySelector(".item");
 
 
 // this changes theme
@@ -38,12 +39,12 @@ addBook.addEventListener("click",() => {
     showAddBook.showModal();
 })
 
+
 showAddBook.addEventListener("click",function(e){
     if(!wrapper.contains(e.target)){
         showAddBook.close();
     }
 })
-
 
 // this closes the dialog box
 cancel.addEventListener("click",function(){
@@ -54,88 +55,3 @@ cancel.addEventListener("click",function(){
 // stores all the books
 const library = [];
 
-
-// this is logic for adding items to itemContainer
-add.addEventListener("click",() => {
-
-
-    function inputValue(author,title,pages){
-        this.author = author;
-        this.title = title;
-        this.pages = pages;
-    }
-
-    // now i know why prototype is so good
-    // these value make sure that same item is not entered more than once
-    inputValue.prototype.entered;
-    inputValue.prototype.check = function(){
-        if(entered === true){
-            return true;
-        }else{
-            false;
-        }
-    }
-
-    function addToLibrary(library){
-        const book = new inputValue(authorD.value,titleD.value,pagesD.value);
-        library.push(book);
-    }
-
-    function showLibrary(){
-        // this code is not perfect and iterate through same items many times
-        // item.entered make sure that it does not happens
-        // i am really proud of this solution
-        library.forEach((item,index) => {
-            const a = document.createElement("p");
-            const p = document.createElement("p");
-            const t = document.createElement("p");
-            // this is for assigning value to div in a patter so that css classes apply accordingly
-            a.textContent = "Author: "+item.author;
-            p.textContent = "Pages: "+item.pages;
-            t.textContent = item.title;
-            const div = document.createElement("div");
-            div.classList.add("item");
-            div.append(p);
-            div.append(t);
-            div.append(a);
-            
-            // this checks if the item being added is unique and not a copy
-            if(!item.entered === true){
-                itemContainer.append(div);
-                console.log("new Item add to library");
-                item.entered = true;
-            }else{
-                console.log("already in the library");
-            }
-        })
-    }
-    addToLibrary(library);
-    showLibrary();
-    clearInput();
-    showAddBook.close();
-})
-
-
-// this was a previour itereation of how i would add books to library
-/* const library = [
-	{
-		user:"name",
-		age:20,
-	}
-]
-
-createEntry.prototype.info = function(){
-	return `${this.user} is of age ${this.age}`;
-}
-
-function createEntry(user,age){
-	this.user = user;
-	this.age = age;
-}
-
-library.push(new createEntry("nanadkljfa",1443));
-library.push(new createEntry("nanadkljfa",1443));
-library.push(new createEntry("nanadkljfa",1443));
-library.push(new createEntry("nanadkljfa",1443));
-library.push(new createEntry("nanadkljfa",1443));
-console.log(library[3].info()) */
